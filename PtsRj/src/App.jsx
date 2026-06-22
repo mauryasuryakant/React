70b93b97840934924017619bc7c767e1e448ba52
Suryakant Maurya <mauryasuryakant99@gmail.com>
2026-06-22 07:17:43 +0000

    first number increase program by myself 😁

--- PtsRj/src/App.jsx
+++ PtsRj/src/App.jsx
@@ -1,16 +1,20 @@
-// import React from 'react'
+import { useState } from "react";
 
 const App = () => {
-  var user = "Ayush";
+  const [counter, setCounter] = useState(1);
 
-  const changeUser = () => {
-    console.log(`Hello, ${user}`)
-  }
+  const runCounter = () => {
+    setCounter(counter + 1);
+  };
 
   return (
-    <main className="bg-black h-screen text-white p-10">
-      <h1 className="text-4xl font-extrabold tracking-tight mb-6">Hello, {user}</h1>
-      <button onClick={changeUser} className="bg-[#20B2AA] hover:bg-[#4b8885] text-white font-medium py-2 px-4 rounded-lg transition colors duration-200">
+    <main className="bg-black h-screen text-[#ffffff] p-10 flex justify-center items-center flex-col">
+      <h1 className="text-6xl font-extrabold tracking-tighter pb-10">{counter}</h1>
+
+      <button
+        onClick={runCounter}
+        className="bg-[#20B2AA] hover:bg-[#4b8885] text-white font-medium py-2 px-4 rounded-lg transition colors duration-200"
+      >
         Change user
       </button>
     </main>
